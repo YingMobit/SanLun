0fe4e43f831100801abfa0ddb2dc3cb9d8736ca7
Merryfling <merryfling@gmail.com>
2024-01-31 16:59:54 +0800

    通用的护盾、血量脚本（基础版）

---
+++ Assets/_Script/Barrier/BarrierHP.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierHP : MonoBehaviour
+{
+    //这个脚本用来写屏障的血量，里面用事件接受并传出被破坏墙数量
+
+    //声明
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
